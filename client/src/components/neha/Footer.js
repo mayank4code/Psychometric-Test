1b84dddc39913f7a9e2945f297d0b88d5b6c1357
neha <nehak78800@gmail.com>
2023-07-14 16:42:53 +0530

    update landing page

---
+++ client/src/components/neha/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './page.css'
+import { BsTwitter } from 'react-icons/bs'
+import { FaFacebook } from 'react-icons/fa'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsYoutube } from 'react-icons/bs'
+import img from './logo512.png'
+
+export const Footer = () => {
+    return (
+        <div className='footer'>
+
+            <div>
+                <img className='footer-img' src={img} />
+            </div>
+            <div>
+                Community <br />
+                About WEP<br />
+                Partners <br />
+            </div>
+
+            <div>
+                Newletter <br />
+                Events<br />
+                Disclaimer<br />
+            </div>
+
+            <div>
+                FAQs<br />
+                Feedback<br />
+            </div>
+
+            <div className='socials'>
+                <a href='https://twitter.com/NITIAayog'><BsTwitter /></a> <br />
+                <a href='https://www.facebook.com/WomenEntrepreneurshipPlatform/'><FaFacebook /></a><br />
+                <a href='https://www.linkedin.com/company/women-entrepreneurship-platform-niti-aayog/' ><BsLinkedin /></a><br />
+                <a href='https://www.youtube.com/watch?v=UFJeTwHh01w' ><BsYoutube /></a><br />
+            </div>
+
+        </div>
+    )
+}
--- client/src/components/neha/Page.js
+++ client/src/components/neha/Page.js
@@ -1,29 +1,149 @@
 import React from 'react'
-import Image from './image.png'
+import Image from './study.png'
 import { Link } from 'react-router-dom'
 import './page.css'
+import Image1 from './stair.png'
+import Image2 from './seam.png'
+import Image3 from './work.png'
+import { Footer } from './Footer'
+import { motion } from 'framer-motion'
+import { InView } from 'react-intersection-observer';
+
+
+
 
 const Page = () => {
   return (<>
 
     <div className='container-page1'>
-<div className='content'>
       <div className='img-container'>
-        <img className='image' src={Image} ></img>
+        <motion.img className='image-top' src={Image}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]
+          }}
+        ></motion.img>
       </div>
 
       <div className='container-2'>
-        <h1 className="text-1">TAKE ONE <br />
-          STEP AHEAD<br />
-          IN YOUR<br />
-          BUSINESS</h1><br />
+        <div className='fade-in-from-right'>
+          <h1 className="text-top">TAKE A<br />
+            STEP AHEAD<br />
+            IN YOUR<br />
+            BUSINESS</h1><br />
 
-        <Link to="/page2" style={{ textDecoration: 'none' }}>
-          <button className='know-btn'>KNOW MORE <span className='arrow'>&rarr;</span></button>
-        </Link>
+          <motion.a href='#start-test' className='know-btn'
+            whileHover={{ scale: 1.1 }} transition={{ type: 'spring', stiffness: 300 }}>
+            KNOW MORE <span className='arrow'>&rarr;</span></motion.a>
+
+        </div>
       </div>
+
+    </div >
+
+    {/* PAGE 2 */}
+
+
+    <section id='start-test' className='container-page2'>
+
+      <div className='wrap'>
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div className='text'
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{duration: 1}}
+            >
+              SELF COMPLIANCE TEST
+            </motion.div>
+          )}
+        </InView>
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{delay:0.5, duration: 1 }}
+            >
+              <img src={Image1} className='image' />
+            </motion.div>
+          )}
+        </InView>
+          
+       
       </div>
-    </div>
+
+      <div className='wrap'>
+      <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{delay:0.5, duration: 1 }}
+            >
+              <img src={Image2} className='image' />
+            </motion.div>
+          )}
+        </InView>
+
+
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div className='text'
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ duration: 1 }}
+            >
+              SELF COMPLIANCE TEST
+            </motion.div>
+          )}
+        </InView>
+      </div>
+
+      <div className='wrap'>
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div className='text'
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ duration: 1 }}
+            >
+              SELF COMPLIANCE TEST
+            </motion.div>
+          )}
+        </InView>
+
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{delay:0.5, duration: 1 }}
+            >
+              <img src={Image3} className='image' />
+            </motion.div>
+          )}
+        </InView>
+      </div>
+
+
+      <div className='btn-wrap'>
+        <Link style={{ textDecoration: 'nwrap' }}>
+          <motion.button className='test-button'
+            whileHover={{ scale: 1.1 }} transition={{ type: 'spring', stiffness: 300 }}
+          >START YOUR TEST<span className='arrow'>&rarr;</span></motion.button></Link>
+      </div>
+    </section>
+
+    <Footer />
+
   </>
   )
 }

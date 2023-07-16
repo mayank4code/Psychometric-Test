3cd429a06dca3bebd5475df59f7cef0bf6746d94
Priyanka verma <priyanka.jat001@gmail.com>
2023-07-16 21:17:31 +0530

    quiz page

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -29,35 +29,58 @@ function Quiz() {
     const [score,setScore] = useState(0);
     const [clickedOption,setClickedOption]=useState(0);
     const [showResult,setShowResult]=useState(false);
+
+    // const [questionIndex, setQuestionIndex] = useState(0);
+
+    // const handleQuestionChange = (newIndex) => {
+    //     setQuestionIndex(newIndex);
+    // }
     
     const changeQuestion = ()=>{
-        updateScore();
+        // updateScore();
         if(currentQuestionIndex< questions.length-1){
             setCurrentQuestionIndex(currentQuestionIndex+1);
             setClickedOption(0);
+            // setQuestionIndex(questionIndex+1);
         }else{
             setShowResult(true)
         }
     }
 
-    const updateScore=()=>{
-        if(clickedOption===1){
-            setScore(score+1);
-        }
-    }
+    // const updateScore=()=>{
+    //     if(clickedOption===1){
+    //         setScore(score+1);
+    //     }
+    // }
     const resetAll=()=>{
         setShowResult(false);
         setCurrentQuestionIndex(0);
         setClickedOption(0);
         setScore(0);
     }
+
+    const imageArray = [require("../images/1.jpg"), require("../images/2.PNG"), require("../images/3.PNG")
+    , require("../images/4.PNG"), require("../images/5.jpg"), require("../images/6.jpg"), require("../images/7.PNG")
+    , require("../images/8.PNG"), require("../images/9.PNG"), require("../images/10.PNG"), require("../images/11.PNG")
+    , require("../images/12.PNG"), require("../images/13.PNG"), require("../images/14.PNG"), require("../images/15.PNG")
+    , require("../images/16.jpg"), require("../images/17.PNG"), require("../images/18.jpg"), require("../images/19.PNG")
+    , require("../images/20.jpg"), require("../images/21.PNG"), require("../images/22.PNG"), require("../images/23.PNG")
+    , require("../images/24.PNG"), require("../images/25.PNG"), require("../images/26.PNG")];
+
+
   return (
     <div className='bodyy'>
     {questions.length===0?"":<>
-    <div>
-        <p className="heading-txt">Quiz APP</p>
+
+    <div className="image-handler">
+
+         <div className="testimage">
+
+        <img src={imageArray[currentQuestionIndex]} alt="img" />
+        </div>
 
     </div>
+
         <div className="container">
             {showResult ? (
                 <QuizResult score={score} totalScore={questions.length} tryAgain={resetAll}/>
